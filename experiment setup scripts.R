7757108cceea6e2e828dfba76434608a2fb4891b
LuckDev <lukedru@yahoo.com>
2025-11-15 15:04:40 -0800

    scripts and such

--- experiment setup scripts.R
+++ experiment setup scripts.R
@@ -7,13 +7,7 @@ randomized_people <- sample(people_blocks, length(people_blocks), replace = FALS
 treatment_conditions <- c("iPhone", "iPad", "Macbook")
 
 experiment_plan <- data.frame(
-  Block = rep(randomized_people, each = length(treatment_conditions) * 5),
-  Treatment = rep(rep(treatment_conditions, each = 5), times = length(people_blocks))
+  Block = rep(randomized_people, each = length(treatment_conditions) * 10),
+  Treatment = rep(rep(treatment_conditions, each = 10), times = length(people_blocks))
 )
-head(experiment_plan, 151)
-
-experiment_plan_v2 <- data.frame(
-  Block = rep(randomized_people, each = length(treatment_conditions) * 5),
-  Treatment = rep(treatment_conditions, times = length(people_blocks) * 5)
-)
-head(experiment_plan_v2, 151)
+head(experiment_plan, 301)
