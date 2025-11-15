caa5aea54e0694da3589110065b440b2eced2c69
LuckDev <lukedru@yahoo.com>
2025-11-15 15:50:12 -0800

    csv yay

--- experiment setup scripts.R
+++ experiment setup scripts.R
@@ -1,15 +1,23 @@
 {r}
 set.seed(11152025)
+
 people_blocks <- c("Stranger 1", "Stranger 2", "Stranger 3", "Stranger 4", "Stranger 5",
                    "Luke", "Aayushi", "Anna", "Joyce", "Karina")
 
 randomized_people <- sample(people_blocks, length(people_blocks), replace = FALSE)
-treatment_conditions <- c("iPhone", "iPad", "Macbook")
 
-experiment_plan <- data.frame(
-  Block = rep(randomized_people, each = length(treatment_conditions) * 10),
-  Treatment = rep(rep(treatment_conditions, each = 10), times = length(people_blocks))
-)
+treatment_conditions <- c("iPhone", "iPad", "Macbook")
+experiment_list <- lapply(randomized_people, function(person) {
+  person_treat_order <- sample(treatment_conditions)
+  
+  data.frame(
+    Block = person,
+    Treatment = rep(person_treat_order, each = 10)
+  )
+})
+
+experiment_plan <- do.call(rbind, experiment_list)
+
 head(experiment_plan, 301)
 
-
+write.csv(experiment_plan, "experiment_plan.csv", row.names = FALSE)
