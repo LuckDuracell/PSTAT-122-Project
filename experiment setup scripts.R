a6a843fd8b212081cc124560c9d7bf4410fd3270
LuckDev <lukedru@yahoo.com>
2025-11-15 15:18:30 -0800

    Update experiment setup scripts.R

--- experiment setup scripts.R
+++ experiment setup scripts.R
@@ -1,7 +1,7 @@
 {r}
 set.seed(11152025)
-people_blocks <- c("Luke", "Aayushi", "Anna", "Joyce", "Karina", 
-                   "Stranger 1", "Stranger 2", "Stranger 3", "Stranger 4", "Stranger 5")
+people_blocks <- c("Stranger 1", "Stranger 2", "Stranger 3", "Stranger 4", "Stranger 5",
+                   "Luke", "Aayushi", "Anna", "Joyce", "Karina")
 
 randomized_people <- sample(people_blocks, length(people_blocks), replace = FALSE)
 treatment_conditions <- c("iPhone", "iPad", "Macbook")
@@ -11,3 +11,5 @@ experiment_plan <- data.frame(
   Treatment = rep(rep(treatment_conditions, each = 10), times = length(people_blocks))
 )
 head(experiment_plan, 301)
+
+
